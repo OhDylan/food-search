d9e88090f684b395e6a404d067365ef6f61aa070
Oh Dylan <ddragon1128@hotmail.com>
2021-11-14 22:40:27 +0800

    Added axios for API search and useEffect hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,16 +1,39 @@
-import React,{ useState } from 'react';
+import React,{ useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
 
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async (searchTerm) => {
+        try {
+
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'san jose'
+                }
+            });
+            setResults(response.data.businesses);
+        } catch(err) {
+            setErrorMessage('Something went wrong.')
+        }
+    }
+
+    useEffect(() => {
+        searchApi('pasta');
+    }, []);
 
     return (
         <View>
-            <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} onTermSubmit={() => console.log('Term submitted')} />
-            <Text>Search Screen</Text>
-            <Text>{term}</Text>
+            <SearchBar term={term} onTermChange={setTerm} onTermSubmit={ () => searchApi(term)} />
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text>We have found {results.length} results.</Text>
         </View>
     );
 };
